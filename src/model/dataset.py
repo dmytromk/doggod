5e437d06008c6bdd66df773a9ed3a31d0c483fd8
Dmytro Mandziuk <dmytromk@gmail.com>
2024-02-25 20:43:16 +0200

    fix folder names

--- src/model/dataset.py
+++ src/model/dataset.py
@@ -9,26 +9,21 @@ def get_all_breeds_url() -> dict[str, list[str]]:
     breeds_list: dict[str, list[str | None]] = get(base_url)["message"]
     result = {}
 
-    for breed, sub_breeds in breeds_list.items():
-        if not sub_breeds:
-            result[breed] = get(f"https://dog.ceo/api/breed/{breed}/images")["message"]
-
-        else:
-            for sub_breed in sub_breeds:
-                result[f"{breed}_{sub_breed}"] = get(f"https://dog.ceo/api/breed/{breed}/{sub_breed}/images")["message"]
+    for breed in breeds_list.keys():
+        result[breed] = get(f"https://dog.ceo/api/breed/{breed}/images")["message"]
 
     return result
 
 
 def download_all_images() -> None:
     breed_urls = get_all_breeds_url()
-    image_folder = RESOURCES_DIR + "/images"
+    image_folder = RESOURCES_DIR + "/dog-api"
 
     for breed, image_urls in breed_urls.items():
-        # Use 80% of the images for training, other - for the validation
+        # Use 60% of the dog-api for training, other - for the validation
         images_amount = len(image_urls)
-        training_proportion = 0.8
-        validation_index = images_amount - int(images_amount * training_proportion)
+        training_proportion = 0.6
+        validation_index = int(images_amount * training_proportion)
 
         for url in image_urls[:validation_index]:
             download_image(url, f"{image_folder}/train/{breed}/{uuid.uuid4().hex}.jpg")
