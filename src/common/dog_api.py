5df3beeea9f691609fdc032f2ee0dc8e93d0947a
Dmytro Mandziuk <dmytromk@gmail.com>
2024-02-27 21:51:11 +0200

    remove images with unsupported file format

--- src/common/dog_api.py
+++ src/common/dog_api.py
@@ -1,4 +1,7 @@
+import imghdr
+import os
 import uuid
+from pathlib import Path
 
 from src.common.config import RESOURCES_DIR
 from src.common.web_utils import get, download_image
@@ -23,3 +26,19 @@ def download_all_images() -> None:
         for url in image_urls:
             if breed == "tervuren" or breed == "vizsla" or breed == "waterdog" or breed == "weimaraner" or breed == "whippet" or breed == "wolfhound":
                 download_image(url, f"{image_folder}/{breed}/{uuid.uuid4().hex}.jpg")
+
+
+def remove_images_unsupported_format() -> None:
+    image_folder = RESOURCES_DIR + "/dog-api"
+    image_extensions = [".jpg"]  # all present images file extensions
+    img_type_accepted_by_tensorflow = ["bmp", "gif", "jpeg", "png"]
+
+    for filepath in Path(image_folder).rglob("*"):
+        if filepath.suffix.lower() in image_extensions:
+            img_type = imghdr.what(filepath)
+            if img_type is None:
+                print(f"{filepath} is not an image")
+                os.remove(filepath)
+            elif img_type not in img_type_accepted_by_tensorflow:
+                print(f"{filepath} is a {img_type}, not accepted by TensorFlow")
+                os.remove(filepath)
