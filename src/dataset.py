64b46f7b97b577ef92252f6e66ab94abab475c1f
Dmytro Mandziuk <dmytromk@gmail.com>
2024-03-01 20:34:10 +0200

    rework data pipeline

--- src/dataset.py
+++ src/dataset.py
@@ -5,48 +5,59 @@ from glob import glob
 import tensorflow as tf
 from tensorflow.python.data.ops.dataset_ops import DatasetV2
 
-from src.common import config
 
-
-def parse_image(filepath: str | bytes, image_size: int):
+def parse_image(filepath: str | bytes, image_size: tuple[int, int]):
     image = tf.io.read_file(filepath)
     image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, [image_size, image_size])
+    image = tf.image.resize(image, image_size)
     image = tf.cast(image, tf.float32) / 255.0
     return image
 
 
 def train_preprocess(image):
     image = tf.image.random_flip_left_right(image)
-
     image = tf.image.random_brightness(image, max_delta=32.0 / 255.0)
     image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
-
     image = tf.clip_by_value(image, 0.0, 1.0)
-
     return image
 
 
-def configure_for_performance(dataset: DatasetV2, batch_size: int) -> DatasetV2:
-    dataset = dataset.shuffle(buffer_size=1000, seed=config.SHUFFLE_SEED)
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.repeat()
-    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-    return dataset
-
-
-def load_dataset(filepath: str, batch_size: int, image_size: int) -> (DatasetV2, int):
+def load_dataset(
+        filepath: str,
+        image_size: tuple[int, int],
+        batch_size: int | None = None,
+        shuffle: bool = True,
+        seed: int = 132,
+        preprocess: bool = False
+) -> DatasetV2:
     classes = os.listdir(filepath)
-    filenames = glob(filepath + '/*/*')
-    random.seed(config.SHUFFLE_SEED)
-    random.shuffle(filenames)
-    labels = [classes.index(name.split('/')[-2]) for name in filenames]
+    image_paths = glob(filepath + '/*/*')
 
-    filenames_ds = tf.data.Dataset.from_tensor_slices(filenames)
-    images_ds = filenames_ds.map(lambda x: parse_image(x, image_size), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    if shuffle:
+        random.seed(seed)
+        random.shuffle(image_paths)
+    labels = [classes.index(name.split('/')[-2]) for name in image_paths]
+
+    filenames_ds = tf.data.Dataset.from_tensor_slices(image_paths)
+    images_ds = filenames_ds.map(lambda x: parse_image(x, image_size), num_parallel_calls=tf.data.AUTOTUNE)
+    if preprocess:
+        images_ds = images_ds.map(train_preprocess, num_parallel_calls=tf.data.AUTOTUNE)
+
     labels_ds = tf.data.Dataset.from_tensor_slices(labels)
+    dataset = tf.data.Dataset.zip((images_ds, labels_ds))
 
-    ds = tf.data.Dataset.zip((images_ds, labels_ds))
-    ds = configure_for_performance(ds, batch_size)
+    if batch_size is not None:
+        if shuffle:
+            dataset = dataset.shuffle(buffer_size=batch_size * 8, seed=seed)
+        dataset = dataset.batch(batch_size)
+    else:
+        if shuffle:
+            dataset = dataset.shuffle(buffer_size=1024, seed=seed)
 
-    return ds, len(filenames)
+    dataset = dataset.prefetch(tf.data.AUTOTUNE)
+
+    dataset.class_names = classes
+
+    dataset.file_paths = image_paths
+
+    return dataset
--- src/model.py
+++ src/model.py
@@ -7,17 +7,14 @@ from src.common.config import RESOURCES_DIR, IMG_SIZE, BATCH_SIZE
 from src.dataset import load_dataset, train_preprocess
 
 
-def build_model(filter_size, num_classes, img_size):
+def build_model(num_classes, img_size):
     model = models.Sequential([
-        layers.Conv2D(filter_size, 3, activation='relu', padding='same', input_shape=(img_size, img_size, 3)),
+        layers.Conv2D(32, 3, activation='relu', padding='same', input_shape=(*img_size, 3)),
         layers.MaxPooling2D(pool_size=2, strides=(2, 2)),
-        layers.Dropout(0.25),
-        layers.Conv2D(filter_size * 2, 3, activation='relu', padding='same'),
+        layers.Conv2D(32, 3, activation='relu', padding='same'),
         layers.MaxPooling2D(pool_size=2, strides=(2, 2)),
-        layers.Dropout(0.25),
         layers.Flatten(),
-        layers.Dense(256, activation='relu'),
-        layers.Dropout(0.5),
+        layers.Dense(512, activation='relu'),
         layers.Dense(num_classes, activation='softmax')
     ])
     model.summary()
@@ -30,36 +27,26 @@ def build_model(filter_size, num_classes, img_size):
     return model
 
 
-def build_train_model(filepath, filter_size, img_size, batch_size):
-    train_dataset, train_size = load_dataset(f"{filepath}/train", batch_size, img_size)
-    valid_dataset, valid_size = load_dataset(f"{filepath}/validation", batch_size, img_size)
-    test_dataset, test_size = load_dataset(f"{filepath}/test", batch_size, img_size)
+def build_train_model(filepath, img_size, batch_size):
+    train_dataset = load_dataset(f"{filepath}/train", img_size, batch_size,
+                                 seed=132, preprocess=True)
+    valid_dataset = load_dataset(f"{filepath}/validation", img_size, batch_size, shuffle=False)
+    test_dataset = load_dataset(f"{filepath}/test", img_size, batch_size, shuffle=False)
 
-    train_dataset = train_dataset.map(lambda image, label: (train_preprocess(image), label),
-                                      num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    print(f"Number of breeds: {len(train_dataset.class_names)}")
 
-    num_classes = len(os.listdir(f"{filepath}/train"))
-
-    print(f"Number of breeds: {num_classes}")
-    print(f"Number of training images: {train_size}")
-    print(f"Number of validation images: {valid_size}")
-    print(f"Number of test images: {test_size}")
-    print(f"Number of images: {test_size + valid_size + train_size}")
-
-    model = build_model(filter_size, num_classes, img_size)
+    model = build_model(len(train_dataset.class_names), img_size)
 
     model.fit(train_dataset,
-              steps_per_epoch=train_size // 32,
+              validation_data=valid_dataset,
               batch_size=batch_size,
               epochs=10,
-              validation_data=valid_dataset,
-              validation_steps=valid_size // 32,
               verbose=1
               )
 
-    test_loss, test_accuracy = model.evaluate(test_dataset, steps=test_size // 32)
+    test_loss, test_accuracy = model.evaluate(test_dataset)
     print(f"Test loss: {test_loss}")
     print(f"Test accuracy: {test_accuracy}")
 
 
-build_train_model(f"{RESOURCES_DIR}/dog-api", 16, IMG_SIZE, BATCH_SIZE)
+build_train_model(f"{RESOURCES_DIR}/dog-api", (IMG_SIZE, IMG_SIZE), BATCH_SIZE)
